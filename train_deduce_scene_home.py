4866f238a1145b290a981eab69a76f65b4e07368
leeorn <padiii@gmail.com>
2020-10-11 21:37:47 -0700

    specify location for supporting files

--- train_deduce_scene_home.py
+++ train_deduce_scene_home.py
@@ -23,60 +23,51 @@ import matplotlib.pyplot as plt
 import resnet
 from config import places_dir
 
-# measure how long it takes to run with Adam
-start_time = time.time()
-
-
-## The name of the models (such as resnet18, AlexNet, etc.)
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
 
-## argparse helps getting parameters from the command line, and print helpful info when needed (such as wrong arg was passed)
-## the importance of this line is to take the model type (defult is resnet 18)
 parser = argparse.ArgumentParser(description='Training of Scene_Only model for the home environment')
 parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
                     help='model architecture: ' +
                         ' | '.join(model_names) +
                         ' (default: resnet18)')
-## the importance of this line is the number of workers which perform multi-process data loading
+
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-## the importance of this line is the number of epocs (the def is 90)
+
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
                     help='number of total epochs to run')
-## if want to start at some epoch number (?) 
+
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-## the importance of this line is the number of batch size
+
 parser.add_argument('-b', '--batch-size', default=128, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-## the importance of this line is the learning rate (currently it's constant)
+
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
-## 
+ 
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-## 
+ 
 parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
-## how often to print information to the scree
+
 parser.add_argument('--print-freq', '-p', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
-## if didn't finish the training, we can resume from the last result. provide path the that epoch
+
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
-## 
+
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_false',
                     help='use pre-trained model')
-## number of classes that are used.
+
 parser.add_argument('--num_classes',default=7, type=int, 
                     help='num of class in the model')
 
-## best_prec1 is for the prediction, if the highest result (the label we predict) is the correct one.
-## for best_prec5 for example, we want that the true label will be within the 5 labels with the highest score
 best_prec1 = 0
 
 def main():
@@ -84,8 +75,6 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    ## which model will be used (for WideResnet/AlexNet we need to modify something, else wt just use the name of the model)
-    # create model
     print("=> creating model '{}'".format(args.arch))
     if args.arch.lower().startswith('wideresnet'):
         # a customized resnet model with last feature map size as 14x14 for better class activation mapping
@@ -114,20 +103,17 @@ def main():
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    ## Some pytorch optimization tool to make training faster. (Yet, it works well only if all the input data is the same size)
     cudnn.benchmark = True
     
     # Data loading code
-    # data_dir = places_dir + '/places365_standard_home'
-    data_dir = places_dir   # check if that works
-    ## inside the data dir I have 2 folders - train & val
+    # data_dir = places_dir     # to run on Leeor's pc 
+    data_dir = places_dir + '/places365_standard_home'
     traindir = os.path.join(data_dir, 'train')
     valdir = os.path.join(data_dir, 'val')
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
-    ## DataLoader provides an iterable over the given dataset
     train_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(traindir, transforms.Compose([
             transforms.RandomSizedCrop(224),
@@ -148,7 +134,6 @@ def main():
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
-    ## define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda()
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
@@ -164,7 +149,6 @@ def main():
 
     accuracies_list = []    
     for epoch in range(args.start_epoch, args.epochs):
-        ## every 30 epochs the learning rate is divided by 10
         adjust_learning_rate(optimizer, epoch)
 
         # train for one epoch
@@ -187,7 +171,6 @@ def main():
     print("The best accuracy obtained during training is = {}".format(best_prec1))
 
 def train(train_loader, model, criterion, optimizer, epoch):
-    ## AverageMeter is a class to store the average current values
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -199,7 +182,6 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
     end = time.time()
     for i, (input, target) in enumerate(train_loader):
-        ## measure data loading time
         data_time.update(time.time() - end)
 
         target = target.cuda(async=True)
@@ -216,11 +198,8 @@ def train(train_loader, model, criterion, optimizer, epoch):
         top5.update(prec5, input.size(0))
 
         # compute gradient and do SGD step
-        ## zero_grad reset the optimizer object to zero to all of the gradients (need to reset every loop)
         optimizer.zero_grad()
-        ## compute gradient of the loss with respect to model param
         loss.backward()
-        ## calling the step function on an Optimizer makes an update to its parameters
         optimizer.step()
 
         # measure elapsed time
@@ -274,7 +253,6 @@ def validate(val_loader, model, criterion):
                     'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                     i, len(val_loader), batch_time=batch_time, loss=losses,
                     top1=top1, top5=top5))
-                print("total time passed until now: ", time.time() - start_time)
 
     print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
           .format(top1=top1, top5=top5))
@@ -285,6 +263,14 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     torch.save(state, filename + '_latest_home.pth.tar')
     if is_best:
         shutil.copyfile(filename + '_latest_home.pth.tar', filename + '_best_home.pth.tar')
+
+# def early_stop(accu, )
+""" Function to determine if to use early stop (the model doesn't learn any more)
+    INPUT:  the accuracy of the current epoch; 
+            the number of epochs we want to wait before terminating (default=7);
+            by how much it needs to improve (default = 0% meaning, any improvement is sufficient) 
+    OUTPUT: True if continue learning (don't stop); False otherwise. """
+# use a stack to store the X accuracies, once the stack reached that amount, if the model doesn't improve, return false
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
